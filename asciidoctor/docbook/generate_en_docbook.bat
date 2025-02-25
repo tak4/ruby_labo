1bb26153afaf611af99ba27c40edbe20f42f749c
tak4 <tak.shi.4@gmail.com>
2025-02-26 01:01:12 +0900

    add docbokk

---
+++ asciidoctor/docbook/generate_en_docbook.bat
@@ -0,0 +1,5 @@
+asciidoctor ^
+    -a lang=en ^
+    -b docbook ^
+    -o en_lang_attribute_sample.xml ^
+    lang_attribute_sample.adoc
---
+++ asciidoctor/docbook/generate_ja_docbook.bat
@@ -0,0 +1,5 @@
+asciidoctor ^
+    -a lang=ja ^
+    -b docbook ^
+    -o ja_lang_attribute_sample.xml ^
+    lang_attribute_sample.adoc
