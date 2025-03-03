cacfbada09ea8288816e3d8cba124cda2b0d57f5
tak4 <tak.shi.4@gmail.com>
2025-03-03 23:07:04 +0900

    modify bat

--- asciidoctor/generate_pdf_costom.bat
+++ asciidoctor/generate_pdf_costom.bat
@@ -1,6 +1,6 @@
 asciidoctor-pdf ^
     -a scripts=cjk ^
-    -a pdf-fontsdir=D:\develop\ruby\asciidoctor\fonts ^
+    -a pdf-fontsdir=fonts ^
     -a pdf-theme=themes\costom_theme.yml ^
     -o costom_theme_sample.pdf ^
     sample.adoc
